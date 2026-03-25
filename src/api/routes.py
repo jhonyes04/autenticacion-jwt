5820b9c98b3c0cb734609a138053859ff15104a2
jhonyes04 <jhonyes@hotmail.com>
2026-03-25 12:56:49 +0000

    Backend terminado

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+import bcrypt
+
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +15,62 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET'])
+def get_users():
+    usuarios = db.session.execute(db.select(User)).scalars().all()
+    return jsonify({'total': len(usuarios), 'users': [user.serialize() for user in usuarios]}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+
+    if not email or not password:
+        return jsonify({'msg': 'Email y password son obligatorios'}), 400
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'msg': 'Usuario ya registrado'}), 400
+
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    nuevo_usuario = User(
+        email=email, password=password_hash.decode('utf-8'), is_active=True)
+
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+
+    return jsonify({'msg': "Usuario creado correctamente", 'user': nuevo_usuario.serialize()}), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'msg': 'No ha proporcionado información para login'}), 400
+
+    email = data['email']
+    password = data['password']
+
+    usuario = User.query.filter_by(email=email).first()
+
+    if usuario and bcrypt.checkpw(password.encode('utf-8'), usuario.password.encode('utf-8')):
+        token = create_access_token(identity=str(usuario.id))
+        return jsonify({'token': token, 'user': usuario.serialize()})
+
+    return jsonify({'msg': 'Email o contraseña incorrectos'}), 401
+
+
+@api.route('/privado', methods=['GET'])
+@jwt_required()
+def privado():
+    id_usuario = get_jwt_identity()
+    usuario = User.query.get(id_usuario)
+
+    return jsonify({
+        'msg': 'Acceso concedido a área privada',
+        'user': usuario.serialize()
+    })
--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -18,6 +19,9 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET") # Cambia esto en .env
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -28,6 +32,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
@@ -57,6 +62,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
